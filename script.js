73230d268b25c2253161957104543dd3ff936735
Shreya Nigam <shreya.nigam1062@gmail.com>
2024-06-19 00:35:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,194 @@
+let turn = "red";
+let stopEvent = false;
+document.querySelector("#red").style.marginLeft = "0vmin";
+document.querySelector("#red").style.marginTop = "0vmin";
+document.querySelector("#blue").style.marginLeft = "0vmin";
+document.querySelector("#blue").style.marginTop = "0vmin";
+
+document.addEventListener("keydown", async (e) => {
+  if (e.keyCode == "83" && !stopEvent) {
+    stopEvent = true;
+    let diceNum = await roll();
+    let isOutOfRange = checkRange(diceNum);
+    await new Promise((resolve) => setTimeout(resolve, 400)); //before run
+    if (!isOutOfRange) {
+      await run(diceNum);
+      await new Promise((resolve) => setTimeout(resolve, 400)); //after run
+    }
+    let wonBy = checkWin()
+    if(wonBy == 'none') {
+      changeTurn()
+      stopEvent = false
+    }
+  }
+});
+
+function checkWin() {
+  if(marginTop() == -72 && marginLeft() == 0) {
+    document.querySelector('#p_turn').innerHTML = `${turn} player wins!`
+    //play audio
+    return turn
+  } else {
+    return 'none'
+  }
+}
+
+function checkRange(diceNum) {
+  let isOutOfRange = false;
+  if (marginTop() == -72 && marginLeft() + Number(diceNum * -8) < 0) {
+    isOutOfRange = true
+  }
+  return isOutOfRange
+}
+
+function changeTurn() {
+  if (turn == "blue") {
+    document.querySelector("#p_turn").innerHTML = "Red player's turn";
+    turn = "red";
+  } else if (turn == "red") {
+    document.querySelector("#p_turn").innerHTML = "Blue player's turn";
+    turn = "blue";
+  }
+}
+
+function run(diceNum) {
+  return new Promise(async (resolve, reject) => {
+    for (i = 1; i <= diceNum; i++) {
+      let direction = getDirection();
+      await move(direction);
+    }
+    await checkLaddersAndSnakes();
+    resolve();
+  });
+}
+
+function checkLaddersAndSnakes() {
+  // [left, top]
+  // 1, 2, 3,  4,  5,  6,  7,  8,  9,  10
+  // 0, 8, 16, 24, 32, 40, 48, 56, 64, 72
+  return new Promise(async (resolve, reject) => {
+    let froms = [
+      [0, -8],
+      [40, -16],
+      [40, -32],
+      [64, -40],
+      [32, -56],
+      [8, -56],
+      [32, -72],
+      [56, -64],
+      [56, 0],
+      [48, -8],
+      [8, -16],
+      [56, -16],
+      [16, -32],
+      [48, -48],
+      [24, -56],
+      [64, -64],
+    ];
+
+    let tos = [
+      [16, 0],
+      [48, 0],
+      [24, 0],
+      [48, -24],
+      [8, -24],
+      [0, -40],
+      [56, -48],
+      [72, -48],
+      [64, -8],
+      [32, -32],
+      [0, -24],
+      [72, -48],
+      [24, -48],
+      [40, -56],
+      [16, -72],
+      [72, -72],
+    ];
+    for (let i = 0; i < tos.length; i++) {
+      if (marginLeft() == froms[i][0] && marginTop() == froms[i][1]) {
+        // play audio
+        document.querySelector(
+          `#${turn}`
+        ).style.marginLeft = `${tos[i][0]}vmin`;
+        document.querySelector(`#${turn}`).style.marginTop = `${tos[i][1]}vmin`;
+        await new Promise((resolve) => setTimeout(resolve, 400));
+        break;
+      }
+    }
+    resolve();
+  });
+}
+
+function move(direction) {
+  console.log(
+    "Top: ",
+    document.querySelector(`#${turn}`).style.marginTop,
+    " Left: ",
+    document.querySelector(`#${turn}`).style.marginLeft
+  );
+  return new Promise(async (resolve, reject) => {
+    // audio play
+    if (direction == "up") {
+      document.querySelector(`#${turn}`).style.marginTop =
+        String(marginTop() - 8) + "vmin";
+    } else if (direction == "right") {
+      document.querySelector(`#${turn}`).style.marginLeft =
+        String(marginLeft() + 8) + "vmin";
+    } else if (direction == "left") {
+      document.querySelector(`#${turn}`).style.marginLeft =
+        String(marginLeft() - 8) + "vmin";
+    }
+    await new Promise((resolve) => setTimeout(resolve, 400));
+    resolve();
+  });
+}
+
+function getDirection() {
+  let direction;
+  if (
+    (marginLeft() == 72 && ((marginTop() * 10) % (-16 * 10)) / 10 == 0) ||
+    (marginLeft() == 0 && ((marginTop() * 10) % (-16 * 10)) / 10 != 0)
+  ) {
+    direction = "up";
+  } else if (((marginTop() * 10) % (-16 * 10)) / 10 == 0) {
+    direction = "right";
+  } else {
+    direction = "left";
+  }
+  return direction;
+}
+
+function marginLeft() {
+  return Number(
+    document.querySelector(`#${turn}`).style.marginLeft.split("v")[0]
+  );
+}
+function marginTop() {
+  return Number(
+    document.querySelector(`#${turn}`).style.marginTop.split("v")[0]
+  );
+}
+
+function roll() {
+  return new Promise(async (resolve, reject) => {
+    let diceNum = Math.floor(Math.random() * 6) + 1;
+    let values = [
+      [0, -360],
+      [-180, -360],
+      [-180, 270],
+      [0, -90],
+      [270, 180],
+      [90, 90],
+    ];
+    new Audio("die.mp3").play();
+    document.querySelector(
+      "#cube_inner"
+    ).style.transform = `rotate(360deg) rotateY(360deg)`;
+    await new Promise((resolve) => setTimeout(resolve, 750));
+    document.querySelector("#cube_inner").style.transform = `rotateX(${
+      values[diceNum - 1][0]
+    }deg) rotateY(${values[diceNum - 1][1]}deg)`;
+    await new Promise((resolve) => setTimeout(resolve, 750));
+    resolve(diceNum);
+  });
+}
